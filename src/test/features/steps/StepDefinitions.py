66e7ba3947b4005e1504e8c43646efab0b4295fa
WalaceCR <walace.dacunha@yahoo.com.br>
2019-04-06 10:40:46 -0300

    Implementing page methods and removing useless steps

--- src/test/features/steps/StepDefinitions.py
+++ src/test/features/steps/StepDefinitions.py
@@ -1,6 +1,6 @@
 from behave import *
 from src.test.util.Util import utilidades
-
+from src.test.pageobject.PaginaInicial import PaginaInicial
 
 @given(u'que o usuário acessa a página')
 def step_impl(context):
@@ -8,19 +8,17 @@ def step_impl(context):
     utilidades.goto_page()
 
 
-@given(u'a caixa de pesquisa existe')
-def step_impl(context):
-    print("a caixa de pesquisa existe")
-
-
 @when(u'digito o conteúdo para pesquisa')
 def step_impl(context):
     print("digito o conteúdo para pesquisa")
+    context.pagina_pesquisa = PaginaInicial(utilidades.get_driver())
+    context.pagina_pesquisa.digitar_pesquisa()
 
 
 @when(u'clico no botão pesquisar')
 def step_impl(context):
     print("clico no botão pesquisar")
+    context.pagina_pesquisa.clicar_em_pesquisa()
 
 
 @then(u'são exibidos os resultados da busca')
