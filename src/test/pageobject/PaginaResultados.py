b4110ae9990b406e81eeba943fa872a57cf3b3dc
WalaceCR <wcrodrigues@edglobo.com.br>
2019-04-12 17:47:25 -0300

    Implementing PaginaResultados class

---
+++ src/test/pageobject/PaginaResultados.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+from src.data.config import settings
+from src.test.util.Util import utilidades
+
+
+class PaginaResultados():
+
+    def __init__(self):
+        pass
+
