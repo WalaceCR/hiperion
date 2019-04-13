0c29eb7d83c02087ef1a96975f49b5c90608af49
WalaceCR <walace.dacunha@yahoo.com.br>
2019-04-13 13:10:27 -0300

    Improving PaginaResultados class

--- src/test/pageobject/PaginaResultados.py
+++ src/test/pageobject/PaginaResultados.py
@@ -5,6 +5,9 @@ from src.test.util.Util import utilidades
 
 class PaginaResultados():
 
-    def __init__(self):
+    def __init__(self, driver):
+        self.resultados = driver.find_element(By.XPATH, "//*[1][@class='g'][1]/div[1]/div[1]/div[1]/a[1]")
         pass
 
+    def clicando_no_item(self):
+        self.resultados.click()
